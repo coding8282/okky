0e051d9d81a80c2bbfbfbb673afe6d08d7fcfded
hyunsu <coding8282@gmail.com>
2018-07-08 17:41:49 +0900

    기타 수정
    - /about -> /miscs/about으로 라우팅 수정.
    - about 설명 수정.

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,9 +5,13 @@
 
         <!-- 링크 -->
         <small>
-            <icon name="info-circle" scale=".80" class="mr-1"/> <nuxt-link to="/about">About</nuxt-link> | 
-            <icon name="code-branch" scale=".80" class="mr-1"/> <a href="https://github.com/coding8282" target="_blank">github</a> | 
-            <icon name="rss" scale=".75" class="mr-1"/> <a href="http://blog.coding8282.com" target="_blank">블로그</a> | 
+          <icon name="info-circle" scale=".80" class="mr-1"/>
+          <nuxt-link to="/miscs/about">About</nuxt-link>
+          |
+          <icon name="code-branch" scale=".80" class="mr-1"/>
+          <a href="https://github.com/coding8282" target="_blank">github</a> |
+          <icon name="rss" scale=".75" class="mr-1"/>
+          <a href="http://blog.coding8282.com" target="_blank">블로그</a> |
             <icon name="smile" scale=".75" class="mr-1"/> <a href="https://facebook.com/coding8282" target="_blank">facebook</a>
         </small>
     </div>
